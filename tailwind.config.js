23fb1327ed49b4242c71bc6c699caacf123434ec
SleeperOpsAi <adam_cantwell@sleeperops.com>
2025-06-14 19:04:51 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in forwards',
+      },
+    },
   },
   plugins: [],
 }
