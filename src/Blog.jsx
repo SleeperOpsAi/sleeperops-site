1401fd3f29ae8231ef3a3471151a930a941bbbaa
SleeperOpsAi <adam_cantwell@sleeperops.com>
2025-06-19 20:10:42 -0500

    Update Blog.jsx

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,8 +1,5 @@
+import SleeperOpsJourney from "./SleeperOpsJourney";
+
 export default function Blog() {
-  return (
-    <div style={{color: "white", padding: "2rem", backgroundColor: "#0a2c4d"}}>
-      <h1>This is a test page for Blog.jsx</h1>
-      <p>If you see this, the routing and deployment are correct.</p>
-    </div>
-  );
+  return <SleeperOpsJourney />;
 }
