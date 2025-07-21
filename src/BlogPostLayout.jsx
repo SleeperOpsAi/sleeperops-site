1a440c78d51a2ce5855f44f0895a06932c44da76
Adam Cantwell <adam_cantwell@sleeperops.com>
2025-07-21 10:13:57 -0500

    Update BlogPostLayout.jsx

--- src/BlogPostLayout.jsx
+++ src/BlogPostLayout.jsx
@@ -60,20 +60,25 @@ export default function BlogPostLayout({ children, title, description, publishDa
         
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="article" />
-        <meta property="og:title" content={fullTitle} />
+        <meta property="og:title" content={title} />
         <meta property="og:description" content={fullDescription} />
         <meta property="og:url" content={currentUrl} />
         <meta property="og:site_name" content="SleeperOps" />
         <meta property="og:image" content={imageUrl} />
+        <meta property="og:image:secure_url" content={imageUrl} />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content={title} />
+        <meta property="fb:app_id" content="your-facebook-app-id" />
         
         {/* Twitter */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content={currentUrl} />
-        <meta property="twitter:title" content={fullTitle} />
-        <meta property="twitter:description" content={fullDescription} />
-        <meta property="twitter:image" content={imageUrl} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@SleeperOps" />
+        <meta name="twitter:creator" content="@SleeperOps" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={fullDescription} />
+        <meta name="twitter:image" content={imageUrl} />
+        <meta name="twitter:url" content={currentUrl} />
         <meta property="twitter:site" content="@SleeperOps" />
         <meta property="twitter:creator" content="@SleeperOps" />
         
